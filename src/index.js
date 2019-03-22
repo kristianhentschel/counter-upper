c6b54f53bfc4c3ae743a93b436dd5266d5f86616
Kristian Hentschel <Kristian.Hentschel@bbc.co.uk>
2019-03-22 20:56:40 +0000

    Revert "Add service worker plugin and service worker copied from example."
    This reverts commit 3d6d270ab2921fccb5d90a9994cab75c74aaba4f.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,9 @@ import { Provider } from 'react-redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
-import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
 import App from './App';
 import reducer from './reducer';
-
-if ('serviceWorker' in navigator) {
-  const registration = runtime.register();
-}
 
 const persistConfig = { key: 'root', storage };
 const persistedReducer = persistReducer(persistConfig, reducer);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 
 module.exports = (env, { mode = 'development' }) => ({
   devtool: 'source-map',
@@ -50,9 +49,6 @@ module.exports = (env, { mode = 'development' }) => ({
     new MiniCssExtractPlugin({
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css',
-    }),
-    new ServiceWorkerWebpackPlugin({
-      entry: path.join(__dirname, 'src/sw.js'),
     }),
   ],
   optimization: {
